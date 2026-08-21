7392fafae688f5bbbf97970ffbaf596c7c8fd184
toby linas <tobyprime0516@outlook.com>
2026-08-21 20:47:20 +0800

    build: unify MTV distribution publishing

---
+++ mcedia-mtv/versions/1.21.8/src/client/java/top/tobyprime/mcedia_mtv/client/EntityPlayerClientEntryPoint.java
@@ -0,0 +1,20 @@
+package top.tobyprime.mcedia_mtv.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import top.tobyprime.mcedia_mtv.client.channel.ClientChannelPlaybackManager;
+import top.tobyprime.mcedia_mtv.client.channel.MtvClientChannelPayloads;
+import top.tobyprime.mcedia_mtv.client.command.MtvHudCommand;
+import top.tobyprime.mcedia_mtv.client.entityplayer.EntityPlayerManager;
+
+public class EntityPlayerClientEntryPoint implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        MtvClientChannelPayloads.register();
+        ClientChannelPlaybackManager.getInstance().clear();
+        MtvHudCommand.register();
+        EntityPlayerManager.getInstance().onInitialize();
+        ClientTickEvents.END_CLIENT_TICK.register(client ->
+                HudChannelPlayer.getInstance().onClientTick());
+    }
+}
